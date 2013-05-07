a9f6a1fd2e50347ed83a9c8fbfcf81c731e2282c
Andreas <andreas@nameonbaby.com>
2013-05-07 15:26:40 +0200

    Added my entire baby-folder

---
+++ WEBSIDOR/NAMN Generator/NameOnMini - Namnförslag/js/gen.js
@@ -0,0 +1,534 @@
+
+var NumberOfWords = 80
+var Tjejnamn = new BuildArray(NumberOfWords)
+
+
+var NumberOfWords1 = 80
+var Tjejnamn_betydelse = new BuildArray(NumberOfWords1)
+
+var NumberOfWords3 = 80
+var Pojknamn = new BuildArray(NumberOfWords3)
+
+
+var NumberOfWords4 = 80
+var Pojknamn_betydelse = new BuildArray(NumberOfWords4)
+
+
+Tjejnamn[1] = "Aagot"
+Tjejnamn_betydelse[1] = "Variant av Ågot, vilket är en norsk form av Agata."
+
+Tjejnamn[2] = "Ada"
+Tjejnamn_betydelse[2] = "Kommer av Adela. Finns även i hebreiskan med betydelsen 'prydnad, smycke'."
+
+Tjejnamn[3] = "Adela"
+Tjejnamn_betydelse[3] = "Tyskt ursprung och betyder 'den ädla'."
+
+Tjejnamn[4] = "Adèle"
+Tjejnamn_betydelse[4] = "Fransk form av Adela."
+
+Tjejnamn[5] = "Adelina"
+Tjejnamn_betydelse[5] = "Släktskap med Adela och Alina."
+
+Tjejnamn[6] = "Adina"
+Tjejnamn_betydelse[6] = "Är i Gamla Testamentet ett mansnamn vilket på hebreiska betyder 'vällust, yppighet'."
+
+Tjejnamn[7] = "Adolfina"
+Tjejnamn_betydelse[7] = "Kvinnoform av tyska Adolf."
+
+Tjejnamn[8] = "Adriana"
+Tjejnamn_betydelse[8] = "Kvinnoform av Adrian."
+
+Tjejnamn[9] = "Adrienne"
+Tjejnamn_betydelse[9] = "Kvinnoform av Adrian."
+
+Tjejnamn[10] = "Agata"
+Tjejnamn_betydelse[10] = "Härstammar från Grekland och är en kvinnoform av Agaton, vilket betyder 'god'."
+
+Tjejnamn[11] = "Agda"
+Tjejnamn_betydelse[11] = "Svensk form av Agata."
+
+Tjejnamn[12] = "Agnes"
+Tjejnamn_betydelse[12] = "Grekiskt ursprung och betyder 'den kyska'. Kan även förknippas med latinska agnus, som betyder 'lamm'."
+
+Tjejnamn[13] = "Agneta"
+Tjejnamn_betydelse[13] = "Latinsk variant av Agnes."
+
+Tjejnamn[14] = "Aila"
+Tjejnamn_betydelse[14] = "Variant av Aili."
+
+Tjejnamn[15] = "Aili"
+Tjejnamn_betydelse[15] = "Finsk-samiskt namn."
+
+Tjejnamn[16] = "Aimée"
+Tjejnamn_betydelse[16] = "Franskt namn som betyder 'älskad'."
+
+Tjejnamn[17] = "Aina"
+Tjejnamn_betydelse[17] = "Variant av det finska namnet Aino."
+
+Tjejnamn[18] = "Aino"
+Tjejnamn_betydelse[18] = "Finsk härkomst med betydelsen 'den enda'."
+
+Tjejnamn[19] = "Aira"
+Tjejnamn_betydelse[19] = "Variant av det finska namnet Airi."
+
+Tjejnamn[20] = "Airi"
+Tjejnamn_betydelse[20] = "Finsk härkomst."
+
+Tjejnamn[21] = "Alba"
+Tjejnamn_betydelse[21] = "Latinskt namn som betyder 'den vita, ljusa'."
+
+Tjejnamn[22] = "Alberta"
+Tjejnamn_betydelse[22] = "Kvinnoform av Albert."
+
+Tjejnamn[23] = "Albertina"
+Tjejnamn_betydelse[23] = "Kommer från Alberta."
+
+Tjejnamn[24] = "Alda"
+Tjejnamn_betydelse[24] = "Tyskt ursprung."
+
+Tjejnamn[25] = "Alexandra"
+Tjejnamn_betydelse[25] = "Kvinnoform av Alexander."
+
+Tjejnamn[26] = "Alfa"
+Tjejnamn_betydelse[26] = "Nordiskt ursprung och är kvinnoform till Alf och Alvar. Associeras även till första bokstaven i grekiska alfabetet, alpha."
+
+Tjejnamn[27] = "Alfhild"
+Tjejnamn_betydelse[27] = "Nordiskt ursprung och är en sammansatt form av Alf och hild, och betyder 'alfernas stridsmö'."
+
+Tjejnamn[28] = "Alfhilda"
+Tjejnamn_betydelse[28] = "Variant av Alfhild."
+
+Tjejnamn[29] = "Alfrida"
+Tjejnamn_betydelse[29] = "Nordiskt ursprung med betydelsen 'mycket vacker'."
+
+Tjejnamn[30] = "Alice"
+Tjejnamn_betydelse[30] = "Engelsk och fransk form av tyska Adelheid som betyder 'av ädel stam'."
+
+Tjejnamn[31] = "Alicia"
+Tjejnamn_betydelse[31] = "Latinsk eller spansk form av Alice."
+
+Tjejnamn[32] = "Alida"
+Tjejnamn_betydelse[32] = "Nederländsk form av tyska Adelheid som betyder 'av ädel stam'."
+
+Tjejnamn[33] = "Alina"
+Tjejnamn_betydelse[33] = "Utveckling av Adelina."
+
+Tjejnamn[34] = "Alli"
+Tjejnamn_betydelse[34] = "Variant av Ally."
+
+Tjejnamn[35] = "Ally"
+Tjejnamn_betydelse[35] = "Är ursprungligen smeknamn till Alexandra och Alice."
+
+Tjejnamn[36] = "Alma"
+Tjejnamn_betydelse[36] = "Spanskt namn som troligen är en kvinnoform av latinska almus som betyder 'närande, mild'."
+
+Tjejnamn[37] = "Altea"
+Tjejnamn_betydelse[37] = "Latinskt namn."
+
+Tjejnamn[38] = "Alva"
+Tjejnamn_betydelse[38] = "Kvinnoform av Alf och Alvar."
+
+Tjejnamn[39] = "Alvi"
+Tjejnamn_betydelse[39] = "Kommer från Alvina."
+
+Tjejnamn[40] = "Alvina"
+Tjejnamn_betydelse[40] = "Kvinnoform av germanska Alvin."
+
+Tjejnamn[41] = "Amalia"
+Tjejnamn_betydelse[41] = "Latinsk form av gotiska Amal, vilket betyder 'verksam'."
+
+Tjejnamn[42] = "Amanda"
+Tjejnamn_betydelse[42] = "Latinskt ursprung med betydelsen 'den älskvärda'."
+
+Tjejnamn[43] = "Amelie"
+Tjejnamn_betydelse[43] = "Fransk form av Amalia."
+
+Tjejnamn[44] = "Amy"
+Tjejnamn_betydelse[44] = "Engelsk form av franska Aimée."
+
+Tjejnamn[45] = "Andrea"
+Tjejnamn_betydelse[45] = "Kvinnoform av Andreas."
+
+Tjejnamn[46] = "Angela"
+Tjejnamn_betydelse[46] = "Italienskt och engelskt namn som betyder 'ängel, den änglalika'."
+
+Tjejnamn[47] = "Angelika"
+Tjejnamn_betydelse[47] = "Latinskt namn som betyder 'änglalik'."
+
+Tjejnamn[48] = "Angelina"
+Tjejnamn_betydelse[48] = "Italiensk variant av Angela."
+
+Tjejnamn[49] = "Anita"
+Tjejnamn_betydelse[49] = "Spanskt och italienskt namn som kommer av Anna. Betyder 'lilla Anna'."
+
+Tjejnamn[50] = "Anja"
+Tjejnamn_betydelse[50] = "Ryskt namn som kommer av Anna."
+
+Tjejnamn[51] = "Ann"
+Tjejnamn_betydelse[51] = "Engelsk och fransk form av Anna."
+
+Tjejnamn[52] = "Annie"
+Tjejnamn_betydelse[52] = "Utvecklat från Ann."
+
+Tjejnamn[53] = "Anna"
+Tjejnamn_betydelse[53] = "Grekisk form av hebreiska Hanna som betyder 'nåd, den benådade'."
+
+Tjejnamn[54] = "Annefrid"
+Tjejnamn_betydelse[54] = "Sammansättning av Anna och Frida."
+
+Tjejnamn[55] = "Annelie"
+Tjejnamn_betydelse[55] = "Kommer från Ann-Louise eller Anna."
+
+Tjejnamn[56] = "Annette"
+Tjejnamn_betydelse[56] = "Franskt namn som kommer från Anna."
+
+Tjejnamn[57] = "Annevi"
+Tjejnamn_betydelse[57] = "Bildat efter typen Gunnvi."
+
+Tjejnamn[58] = "Anngerd"
+Tjejnamn_betydelse[58] = "Bildat efter typen Ingegerd."
+
+Tjejnamn[59] = "Annika"
+Tjejnamn_betydelse[59] = "Tyskt namn som kommer av Anna och betyder 'den lilla nåderika'."
+
+Tjejnamn[60] = "Annmari"
+Tjejnamn_betydelse[60] = "Sammansättning av Anna och Maria"
+
+Tjejnamn[61] = "Annvor"
+Tjejnamn_betydelse[61] = "Bildat efter typen Gunvor."
+
+Tjejnamn[62] = "Ansa"
+Tjejnamn_betydelse[62] = "Kommer från Anna-Lisa."
+
+Tjejnamn[63] = "Antoinetta"
+Tjejnamn_betydelse[63] = "Franskt ursprung som är kvinnoform till Anton."
+
+Tjejnamn[64] = "Antoinette"
+Tjejnamn_betydelse[64] = "Franskt ursprung som är kvinnoform till Anton."
+
+Tjejnamn[65] = "Antonia"
+Tjejnamn_betydelse[65] = "Franskt ursprung som är kvinnoform till Anton och betyder 'den oskattbara'."
+
+Tjejnamn[66] = "Arja"
+Tjejnamn_betydelse[66] = "Finskt namn och kommer av Irja."
+
+Tjejnamn[67] = "Arla"
+Tjejnamn_betydelse[67] = "Ett nordiskt namn som förknippas med ordet arla som betyder 'tidig'."
+
+Tjejnamn[68] = "Arna"
+Tjejnamn_betydelse[68] = "Nordiskt namn som uppfattas som kvinnoform till Arne."
+
+Tjejnamn[69] = "Arnevi"
+Tjejnamn_betydelse[69] = "Bildat efter typen Hillevi."
+
+Tjejnamn[70] = "Arvida"
+Tjejnamn_betydelse[70] = "Kvinnoform av Arvid."
+
+Tjejnamn[71] = "Aslög"
+Tjejnamn_betydelse[71] = "Nordiskt ursprung."
+
+Tjejnamn[72] = "Asta"
+Tjejnamn_betydelse[72] = "Kommer av Astrid."
+
+Tjejnamn[73] = "Astrid"
+Tjejnamn_betydelse[73] = "Nordiskt namn som är sammansatt av as (gud) och frid, vilket betyder 'skön, älskad'."
+
+Tjejnamn[74] = "Augusta"
+Tjejnamn_betydelse[74] = "Kvinnoform av Augustus som betyder 'den vördnadsvärda'."
+
+Tjejnamn[75] = "Aulinkki"
+Tjejnamn_betydelse[75] = "Finskt ursprung."
+
+Tjejnamn[76] = "Aune"
+Tjejnamn_betydelse[76] = "Kommer av Aunetta som är en finsk form av Agneta."
+
+Tjejnamn[77] = "Aurelia"
+Tjejnamn_betydelse[77] = "Romerskt ursprung."
+
+Tjejnamn[78] = "Aurora"
+Tjejnamn_betydelse[78] = "Grekiskt ursprung."
+
+Tjejnamn[79] = "Axelia"
+Tjejnamn_betydelse[79] = "Kvinnoform av Axel."
+
+Tjejnamn[80] = "Axelia"
+Tjejnamn_betydelse[80] = "Kvinnoform av Axel."
+
+
+
+
+
+
+
+
+
+
+
+Pojknamn[1] = "Abbe"
+Pojknamn_betydelse[1] = "Kommer från Abraham, Albert och Albin."
+
+Pojknamn[2] = "Abdon"
+Pojknamn_betydelse[2] = "Hebreiskt namn som betyder 'tjänare, fader till många'."
+
+Pojknamn[3] = "Abel"
+Pojknamn_betydelse[3] = "Grekisk form av hebreiska Hevel, vilket betyder 'vindfläkt, förgänglighet'."
+
+Pojknamn[4] = "Abraham"
+Pojknamn_betydelse[4] = "Hebreiskt namn som betyder 'folkets fader'."
+
+Pojknamn[5] = "Adam"
+Pojknamn_betydelse[5] = "Hebreiskt namn som betyder 'människa'."
+
+Pojknamn[6] = "Adel"
+Pojknamn_betydelse[6] = "Släktskap med Adolf."
+
+Pojknamn[7] = "Adils"
+Pojknamn_betydelse[7] = "Fornnordiskt namn som betyder 'ädelt spjut'."
+
+Pojknamn[8] = "Adin"
+Pojknamn_betydelse[8] = "Mansform av Adela, vilket betyder 'den ädla'."
+
+Pojknamn[9] = "Adolf"
+Pojknamn_betydelse[9] = "Tyskt namn som betyder 'ädel varg'."
+
+Pojknamn[10] = "Adrian"
+Pojknamn_betydelse[10] = "Latinskt namn som kommer av Hadrianus, vilket betyder 'man från staden Adria'."
+
+Pojknamn[11] = "Agaton"
+Pojknamn_betydelse[11] = "Grekiskt namn som betyder 'den gode'."
+
+Pojknamn[12] = "Agnar"
+Pojknamn_betydelse[12] = "Fornsvenskt namn som betyder 'hård kämpe'."
+
+Pojknamn[13] = "Agne"
+Pojknamn_betydelse[13] = "Fornsvenskt namn som betyder 'hård kämpe'."
+
+Pojknamn[14] = "Ahmad"
+Pojknamn_betydelse[14] = "Har arabiskt ursprung och betyder 'lovvärd'."
+
+Pojknamn[15] = "Ahmed"
+Pojknamn_betydelse[15] = "Har arabiskt ursprung och betyder 'lovvärd'."
+
+Pojknamn[16] = "Aimo"
+Pojknamn_betydelse[16] = "Finsk-samiskt namn som betyder 'duktig, förträfflig, viktig, äkta, ständig, luft'."
+
+Pojknamn[17] = "Alban"
+Pojknamn_betydelse[17] = "Latinskt namn som betyder 'man från staden Alba'."
+
+Pojknamn[18] = "Albert"
+Pojknamn_betydelse[18] = "Tyskt namn som kommer av Adalbert, vilket betyder 'den förnäme'."
+
+Pojknamn[19] = "Albin"
+Pojknamn_betydelse[19] = "Latinskt ursprung och betyder 'den ljuse'."
+
+Pojknamn[20] = "Albrekt"
+Pojknamn_betydelse[20] = "Tyskt namn som kommer av Albert."
+
+Pojknamn[21] = "Aldor"
+Pojknamn_betydelse[21] = "Ursprung och betydelse okänt."
+
+Pojknamn[22] = "Alex"
+Pojknamn_betydelse[22] = "Kommer av Alexander."
+
+Pojknamn[23] = "Alexander"
+Pojknamn_betydelse[23] = "Grekiskt namn som betyder 'människornas hjälpare'."
+
+Pojknamn[24] = "Alexis"
+Pojknamn_betydelse[24] = "Latinsk form av grekiska Alexos, och betyder 'försvarare'."
+
+Pojknamn[25] = "Alexius"
+Pojknamn_betydelse[25] = "Kommer av Alexis."
+
+Pojknamn[26] = "Alf"
+Pojknamn_betydelse[26] = "Nordiskt namn."
+
+Pojknamn[27] = "Alfons"
+Pojknamn_betydelse[27] = "Germanskt namn som betyder 'den ädle'."
+
+Pojknamn[28] = "Alfred"
+Pojknamn_betydelse[28] = "Engelskt namn som betyder 'rådd av gudarna, alfernas rådgivare'."
+
+Pojknamn[29] = "Algot"
+Pojknamn_betydelse[29] = "Svenskt namn med möjlig betydelse 'den ädle goten'."
+
+Pojknamn[30] = "Ali"
+Pojknamn_betydelse[30] = "Arabiskt namn som betyder 'den upphöjde, den ädle'."
+
+Pojknamn[31] = "Allan"
+Pojknamn_betydelse[31] = "Engelskt namn med möjlig betydelse 'den harmoniske'."
+
+Pojknamn[32] = "Almar"
+Pojknamn_betydelse[32] = "Kan vara mansform till Alma eller komma från ett forntyskt ord med betydelsen 'ädel, berömd'."
+
+Pojknamn[33] = "Alrik"
+Pojknamn_betydelse[33] = "Fornnordiskt namn som betyder 'allhärskare'."
+
+Pojknamn[34] = "Alvar"
+Pojknamn_betydelse[34] = "Nordiskt ursprung."
+
+Pojknamn[35] = "Alve"
+Pojknamn_betydelse[35] = "Kommer av Alf."
+
+Pojknamn[36] = "Alvin"
+Pojknamn_betydelse[36] = "Germanskt namn."
+
+Pojknamn[37] = "Amandus"
+Pojknamn_betydelse[37] = "latisnkt namn som betyder 'den älskvärde'."
+
+Pojknamn[38] = "Ambjörn"
+Pojknamn_betydelse[38] = "Fornsvenskt namn."
+
+Pojknamn[39] = "Ambrosius"
+Pojknamn_betydelse[39] = "Grekiskt namn som betyder 'den odödlige'."
+
+Pojknamn[40] = "Amir"
+Pojknamn_betydelse[40] = "Arabiskt namn som betyder 'härskare, prins'."
+
+Pojknamn[41] = "Anders"
+Pojknamn_betydelse[41] = "Nordisk variant av Andreas."
+
+Pojknamn[42] = "André"
+Pojknamn_betydelse[42] = "Fransk variant av Andreas."
+
+Pojknamn[43] = "Andreas"
+Pojknamn_betydelse[43] = "Grekiskt namn som betyder 'man, den manlige'."
+
+Pojknamn[44] = "Andres"
+Pojknamn_betydelse[44] = "Variant av Anders."
+
+Pojknamn[45] = "Andrew"
+Pojknamn_betydelse[45] = "Angelsk variant av Andreas."
+
+Pojknamn[46] = "Andrzej"
+Pojknamn_betydelse[46] = "Polsk variant av Andreas."
+
+Pojknamn[47] = "Anselm"
+Pojknamn_betydelse[47] = "Tyskt namn som kommer av Ansehelm, vilket betyder 'gudahjälm'."
+
+Pojknamn[48] = "Ansgar"
+Pojknamn_betydelse[48] = "Forntyskt namn som betyder 'gudaspjut'."
+
+Pojknamn[49] = "Ante"
+Pojknamn_betydelse[49] = "Kommer av Anders och Anton"
+
+Pojknamn[50] = "Antero"
+Pojknamn_betydelse[50] = "Finsk variant av Andreas."
+
+Pojknamn[51] = "Anthony"
+Pojknamn_betydelse[51] = "Engelsk variant av Antonius och Anton."
+
+Pojknamn[52] = "Anton"
+Pojknamn_betydelse[52] = "Förkortning av Antonius och betyder 'den ovärderlige'."
+
+Pojknamn[53] = "Antonio"
+Pojknamn_betydelse[53] = "Italiensk variant av Antonius."
+
+Pojknamn[54] = "Antonius"
+Pojknamn_betydelse[54] = "Latinskt ursprung."
+
+Pojknamn[55] = "Antti"
+Pojknamn_betydelse[55] = "Finsk variant av Andreas."
+
+Pojknamn[56] = "Anund"
+Pojknamn_betydelse[56] = "Nordiskt ursprung."
+
+Pojknamn[57] = "Arent"
+Pojknamn_betydelse[57] = "Tysk variant av Arnold."
+
+Pojknamn[58] = "Ari"
+Pojknamn_betydelse[58] = "Nordiskt och hebresikt namn med betydelsen 'örn' respektive 'lejon'."
+
+Pojknamn[59] = "Arild"
+Pojknamn_betydelse[59] = "Nordisk variant av Arnold."
+
+Pojknamn[60] = "Armas"
+Pojknamn_betydelse[60] = "Finskt namn som betyder 'älsklig, trofast'."
+
+Pojknamn[61] = "Arne"
+Pojknamn_betydelse[61] = "Nordiskt namn."
+
+Pojknamn[62] = "Arnold"
+Pojknamn_betydelse[62] = "Tyskt namn med möjlig betydelse 'stark som en örn'."
+
+Pojknamn[63] = "Aron"
+Pojknamn_betydelse[63] = "Hebreiskt namn med möjlig betydelse 'upplyst, bergsman'."
+
+Pojknamn[64] = "Arto"
+Pojknamn_betydelse[64] = "Finskt ursprung."
+
+Pojknamn[65] = "Artur"
+Pojknamn_betydelse[65] = "Engelskt namn med möjlig betydelse 'stark som en björn'."
+
+Pojknamn[66] = "Arvid"
+Pojknamn_betydelse[66] = "Nordiskt namn."
+
+Pojknamn[67] = "Arvo"
+Pojknamn_betydelse[67] = "Finskt ursprung."
+
+Pojknamn[68] = "Assar"
+Pojknamn_betydelse[68] = "Nordiskt namn som kommer av Anaswar, vilket betyder 'den som svarar'. "
+
+Pojknamn[69] = "Aste"
+Pojknamn_betydelse[69] = "Mansform av Asta."
+
+Pojknamn[70] = "Astor"
+Pojknamn_betydelse[70] = "Angloamerikanskt namn med anknytning till nordiska namn."
+
+Pojknamn[71] = "Atle"
+Pojknamn_betydelse[71] = "Fornnordiskt namn."
+
+Pojknamn[72] = "August"
+Pojknamn_betydelse[72] = "Latinskt namn som betyder 'den vördnadsvärde'."
+
+Pojknamn[73] = "Augustin"
+Pojknamn_betydelse[73] = "Latinskt namn."
+
+Pojknamn[74] = "Aulis"
+Pojknamn_betydelse[74] = "Finskt namn som betyder 'givmild, generös, hjälpsam, godhjärtad mot fattiga'."
+
+Pojknamn[75] = "Axel"
+Pojknamn_betydelse[75] = "Nordisk variant av bibliska Absalon, vilket betyder 'fadern är välgång'. "
+
+
+
+
+
+
+
+
+function BuildArray(size){
+    this.length = size
+    for (var i = 1; i <= size; i++){
+        this[i] = null}
+    return this
+}
+
+function PickRandomWord(frm) {
+     var rnd = Math.ceil(Math.random() * NumberOfWords)
+
+    frm.WordBox.value = Tjejnamn[rnd]
+}
+
+
+function PickRandomWord1(frm) {
+     var rnd = Math.ceil(Math.random() * NumberOfWords1)
+
+    frm.WordBox1.value = Tjejnamn_betydelse[rnd]
+}
+
+
+
+
+
+function PickRandomWord3(frm) {
+     var rnd = Math.ceil(Math.random() * NumberOfWords3)
+
+    frm.WordBox.value = Pojknamn[rnd]
+}
+
+
+function PickRandomWord4(frm) {
+     var rnd = Math.ceil(Math.random() * NumberOfWords4)
+
+    frm.WordBox1.value = Pojknamn_betydelse[rnd]
+}
